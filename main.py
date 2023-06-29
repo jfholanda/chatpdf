47f3db2845b94306b8e997e1930e7b1d29e5fc95
jfholanda <joaofelipeholanda@gmail.com>
2023-06-29 20:38:25 -0300

    featuring code

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import streamlit as st
 import PyPDF2
-import Langchain
+import langchain
+import pickle
+import os
+
+from dotenv import load_dotenv 
 
 from streamlit_extras.add_vertical_space import add_vertical_space
 
 from PyPDF2 import PdfReader
 
-from Langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.llms import OpenAI 
+from langchain.chains.question_answering import load_qa_chain
+
+load_dotenv()
+
+# API Key da OpenAI
+os.environ["OPENAI_API_KEY"] = os.getenv("API_KEY")
 
 with st.sidebar:
     st.title("ChatPDF")
@@ -45,8 +58,36 @@ def main():
             conteudo += conteudo_pagina
 
         # Criando os chunks
-        
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size = 1000,
+            chunk_overlap = 200,
+            length_function = len
+        )
+        chunks = text_splitter.split_text(conteudo)
 
+        # Verificando a existência do vetor semântico
+        filename = file.name[:-4]
+        if os.path.exists(f"{filename}.pkl"):
+            with open(file=f"{filename}.pkl", mode='rb') as pdf_file:
+                vectorstore = pickle.load(pdf_file)
+        else:
+            embedding = OpenAIEmbeddings()
+            vectorstore = FAISS.from_texts(texts=chunks, embedding=embedding)
+            with open(file=f"{filename}.pkl", mode='wb') as pdf_file:
+                pickle.dump(vectorstore, pdf_file)
+
+        # Recebendo pergunta
+        pergunta = st.text_input(label="Faça uma pergunta")
+
+        if pergunta:
+            paginas_semanticas = vectorstore.similarity_search(query=pergunta)
+
+            llm = OpenAI(temperature=0, model_name = "gpt-3.5-turbo")
+            chain = load_qa_chain(llm=llm, chain_type="stuff")
+            resposta = chain.run(input_documents=paginas_semanticas, 
+            question=pergunta)
+
+            st.write(resposta)
 
 if __name__ == "__main__":
     main()
